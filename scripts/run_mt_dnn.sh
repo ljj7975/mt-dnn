369ab4ee394d73986cde69a120ad924f734132a9
Jaejun Lee <ljj7975@gmail.com>
2019-11-16 21:17:41 -0500

    supporting roberta base model training

--- scripts/run_mt_dnn.sh
+++ scripts/run_mt_dnn.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [[ $# -ne 3 ]]; then
-  echo "train.sh <batch_size> <gpu> <'base'/'large'>"
+  echo "train.sh  <'bert_base'/'bert_large'/'roberta_base'/'roberta_large'> <batch_size> <gpu>"
   exit 1
 fi
 prefix="mt-dnn-rte"
@@ -14,13 +14,19 @@ train_datasets="mnli,rte,qqp,qnli,mrpc,sst,cola,stsb"
 test_datasets="mnli_matched,mnli_mismatched,rte"
 MODEL_ROOT="checkpoints"
 
-if [[ $3 == "base" ]]
+echo $3
+if [[ $3 == "bert_base" ]]
 then
-	echo "BASE MODEL"
 	BERT_PATH="mt_dnn_models/bert_model_base_uncased.pt"
-else
-	echo "LARGE MODEL"
+elif [[ $3 == "bert_large" ]]
 	BERT_PATH="mt_dnn_models/bert_model_large_uncased.pt"
+elif [[ $3 == "roberta_base" ]]
+	BERT_PATH="mt_dnn_models/roberta.base/model.pt"
+elif [[ $3 == "roberta_large" ]]
+	BERT_PATH="mt_dnn_models/roberta.large/model.pt"
+else
+    echo "INCORRECT MODEL NAME"
+    exit 1
 fi
 
 DATA_DIR="data/canonical_data/bert_uncased_lower"
@@ -31,6 +37,6 @@ grad_clipping=0
 global_grad_clipping=1
 lr="5e-5"
 
-model_dir="checkpoints/${3}_${prefix}_${optim}_answer_opt${answer_opt}_gc${grad_clipping}_ggc${global_grad_clipping}_${tstr}"
+model_dir="checkpoints/${3}_${tstr}"
 log_file="${model_dir}/log.log"
 python train.py --data_dir ${DATA_DIR} --init_checkpoint ${BERT_PATH} --batch_size ${BATCH_SIZE} --output_dir ${model_dir} --log_file ${log_file} --answer_opt ${answer_opt} --optimizer ${optim} --train_datasets ${train_datasets} --test_datasets ${test_datasets} --grad_clipping ${grad_clipping} --global_grad_clipping ${global_grad_clipping} --learning_rate ${lr} --multi_gpu_on
