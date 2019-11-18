74f9c67734e19fce78c8547049db15c98d9c704b
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-11-18 18:02:55 -0500

    minor fix in the scripts

--- cc_scripts/ontario_scripts/train_bert_large.sh
+++ cc_scripts/ontario_scripts/train_bert_large.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=4-00:00:00
 #SBATCH --gres=gpu:v100:4
 #SBATCH --cpus-per-task=4
-#SBATCH --output=mt_dnn_large.out
+#SBATCH --output=mt_dnn_bert_large.out
 #SBATCH --mem=64G
 
 bash scripts/run_mt_dnn.sh bert_large 16 0,1,2,3
