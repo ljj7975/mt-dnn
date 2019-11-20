1446d59aed31cc0adb27783642b06744de1e1e99
Brandon Lee <j474lee@edu.uwaterloo.ca>
2019-11-20 09:48:06 -0500

    utilizing 2 more gpus for large models

--- cc_scripts/train_roberta_large.sh
+++ cc_scripts/train_roberta_large.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --account=def-jimmylin
-#SBATCH --time=6-00:00:00
-#SBATCH --gres=gpu:v100:2
+#SBATCH --time=7-00:00:00
+#SBATCH --gres=gpu:v100:4
 #SBATCH --cpus-per-task=4
 #SBATCH --output=mt_dnn_roberta_large.out
 #SBATCH --mem=64G
 
 conda activate brandon_mt-dnn
 
-bash scripts/run_mt_dnn.sh roberta_large 8 0,1
+bash scripts/run_mt_dnn.sh roberta_large 8 0,1,2,3
